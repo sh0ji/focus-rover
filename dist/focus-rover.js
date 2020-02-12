02b7c44d7cc63a6d6f77e0bc13076977106b1f69
Evan Yamanishi <sh0ji@users.noreply.github.com>
2020-02-12 18:22:32 -0500

    chore: build dist

--- dist/focus-rover.js
+++ dist/focus-rover.js
@@ -1,77 +1,3 @@
-var classCallCheck = function (instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-};
-
-var createClass = function () {
-  function defineProperties(target, props) {
-    for (var i = 0; i < props.length; i++) {
-      var descriptor = props[i];
-      descriptor.enumerable = descriptor.enumerable || false;
-      descriptor.configurable = true;
-      if ("value" in descriptor) descriptor.writable = true;
-      Object.defineProperty(target, descriptor.key, descriptor);
-    }
-  }
-
-  return function (Constructor, protoProps, staticProps) {
-    if (protoProps) defineProperties(Constructor.prototype, protoProps);
-    if (staticProps) defineProperties(Constructor, staticProps);
-    return Constructor;
-  };
-}();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var toConsumableArray = function (arr) {
-  if (Array.isArray(arr)) {
-    for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i];
-
-    return arr2;
-  } else {
-    return Array.from(arr);
-  }
-};
-
 /**
  * Set a roving focus on a group of elements.
  * @author Evan Yamanishi
@@ -87,7 +13,7 @@ var Default = {
     horizontal: true,
     wrap: true,
     initialFocusIndex: 0,
-    resetOnExit: false
+    resetOnExit: false,
 };
 
 /**
@@ -101,140 +27,143 @@ var Forward = Symbol('forward');
  * The main class
  * @type {Class}
  */
-
-var Rover = function () {
-    /**
-     * [constructor description]
-     * @param  {Array} focusableElements An array of focusable elements.
-     * @param  {Object} [config]         An optional configuration.
-     */
-    function Rover(focusableElements, config) {
-        classCallCheck(this, Rover);
-
-        this.elements = focusableElements;
-        this.default = Default;
-        this.config = Object.assign({}, Default, config);
-        if (this.elements.length) this.init(true);
+var Rover = function Rover(focusableElements, _config) {
+    var elements = focusableElements || [];
+    var config = _config;
+    if (typeof focusableElements === 'object') {
+        if (focusableElements instanceof NodeList) {
+            elements = Array.from(focusableElements);
+        } else {
+            elements = [];
+            config = focusableElements;
+        }
     }
+    this.elements = elements;
+    this.default = Default;
+    this.config = Object.assign({}, Default, config);
+    if (this.elements.length) { this.init(true); }
+};
 
-    /**
-     * Initialize the element
-     * @param {Boolean} setListeners Option to set the event listeners.
-     */
+var prototypeAccessors = { currentElement: { configurable: true },backward: { configurable: true },forward: { configurable: true },lastIndex: { configurable: true },validKeys: { configurable: true } };
 
+/**
+ * Initialize the element
+ * @param {Boolean} setListeners Option to set the event listeners.
+ */
+Rover.prototype.init = function init (setListeners) {
+        var this$1 = this;
 
-    createClass(Rover, [{
-        key: 'init',
-        value: function init(setListeners) {
-            var _this = this;
+    this.elements.forEach(function (el, i) {
+        this$1.initTabindex(el, i);
+        if (setListeners) { this$1.setListeners(el, i); }
+    });
+    return this;
+};
 
-            this.elements.forEach(function (el, i) {
-                _this.initTabindex(el, i);
-                if (setListeners) _this.setListeners(el, i);
-            });
-            return this;
+/**
+ * Initialize tabindex values on the element
+ * @param  {Object} el The element.
+ * @param  {Number} i  The element's index in the collection.
+ */
+Rover.prototype.initTabindex = function initTabindex (el, i) {
+    if (i === this.config.initialFocusIndex) {
+        if (el.getAttribute('href')) {
+            el.removeAttribute('tabindex');
+        } else {
+            el.setAttribute('tabindex', 0);
         }
+    } else {
+        el.setAttribute('tabindex', -1);
+    }
+    return this;
+};
 
-        /**
-         * Initialize tabindex values on the element
-         * @param  {Object} el The element.
-         * @param  {Number} i  The element's index in the collection.
-         */
+Rover.prototype.setListeners = function setListeners (el, i) {
+        var this$1 = this;
 
-    }, {
-        key: 'initTabindex',
-        value: function initTabindex(el, i) {
-            if (i === this.config.initialFocusIndex) {
-                if (el.getAttribute('href')) {
-                    el.removeAttribute('tabindex');
-                } else {
-                    el.setAttribute('tabindex', 0);
-                }
-            } else {
-                el.setAttribute('tabindex', -1);
-            }
-            return this;
+    var notEl = this.elements.filter(function (elem) { return elem !== el; });
+    el.addEventListener('keydown', function (e) {
+        if (this$1.validKeys.includes(e.code)) {
+            e.preventDefault();
+            var nextIndex = this$1.nextIndex(i, this$1.forward.includes(e.code));
+            this$1.elements[nextIndex].focus();
         }
-    }, {
-        key: 'setListeners',
-        value: function setListeners(el, i) {
-            var _this2 = this;
+    });
+    el.addEventListener('focusin', function (e) {
+        if (e.target !== this$1.currentElement) {
+            Rover.activate(el);
+            notEl.forEach(function (elem) { return Rover.deactivate(elem); });
+        }
+    });
+    el.addEventListener('blur', function (e) {
+        var newFocus = e.relatedTarget;
+        if (this$1.config.resetOnExit
+            && !this$1.elements.includes(newFocus)) {
+            this$1.init(false);
+        }
+    });
+    return this;
+};
 
-            el.addEventListener('keydown', function (e) {
-                if (_this2.validKeys.includes(e.code)) {
-                    e.preventDefault();
-                    var nextIndex = _this2.nextIndex(i, _this2.forward.includes(e.code));
-                    Rover.moveFocus(el, _this2.elements[nextIndex]);
-                }
-            });
-            el.addEventListener('blur', function (e) {
-                var newFocus = e.relatedTarget;
-                if (_this2.config.resetOnExit && ![].concat(toConsumableArray(_this2.elements)).includes(newFocus)) {
-                    _this2.init(false);
-                }
-            });
-            return this;
+Rover.prototype.nextIndex = function nextIndex (currentIndex, moveForward) {
+    if (moveForward) {
+        if (currentIndex === this.lastIndex) {
+            if (this.config.wrap) { return 0; }
+            return currentIndex;
         }
-    }, {
-        key: 'nextIndex',
-        value: function nextIndex(currentIndex, moveForward) {
-            if (moveForward) {
-                if (currentIndex === this.lastIndex) {
-                    if (this.config.wrap) return 0;
-                    return currentIndex;
-                }
-                return currentIndex + 1;
-            }
-            if (currentIndex === 0) {
-                if (this.config.wrap) return this.lastIndex;
-                return currentIndex;
-            }
-            return currentIndex - 1;
-        }
-    }, {
-        key: 'backward',
-        get: function get$$1() {
-            if (!this[Backward]) {
-                this[Backward] = [];
-                if (this.config.vertical) this[Backward].push('ArrowUp');
-                if (this.config.horizontal) this[Backward].push('ArrowLeft');
-            }
-            return this[Backward];
-        }
-    }, {
-        key: 'forward',
-        get: function get$$1() {
-            if (!this[Forward]) {
-                this[Forward] = [];
-                if (this.config.vertical) this[Forward].push('ArrowDown');
-                if (this.config.horizontal) this[Forward].push('ArrowRight');
-            }
-            return this[Forward];
-        }
-    }, {
-        key: 'lastIndex',
-        get: function get$$1() {
-            return this.elements.length - 1;
-        }
-    }, {
-        key: 'validKeys',
-        get: function get$$1() {
-            return this.backward.concat(this.forward) || [];
-        }
-    }], [{
-        key: 'moveFocus',
-        value: function moveFocus(from, to) {
-            from.setAttribute('tabindex', -1);
-            if (to.getAttribute('href')) {
-                to.removeAttribute('tabindex');
-            } else {
-                to.setAttribute('tabindex', 0);
-            }
-            to.focus();
-            return this;
-        }
-    }]);
-    return Rover;
-}();
+        return currentIndex + 1;
+    }
+    if (currentIndex === 0) {
+        if (this.config.wrap) { return this.lastIndex; }
+        return currentIndex;
+    }
+    return currentIndex - 1;
+};
+
+prototypeAccessors.currentElement.get = function () {
+    return this.elements.find(function (el) { return el.getAttribute('tabindex') !== '-1'; });
+};
+
+prototypeAccessors.backward.get = function () {
+    if (!this[Backward]) {
+        this[Backward] = [];
+        if (this.config.vertical) { this[Backward].push('ArrowUp'); }
+        if (this.config.horizontal) { this[Backward].push('ArrowLeft'); }
+    }
+    return this[Backward];
+};
+
+prototypeAccessors.forward.get = function () {
+    if (!this[Forward]) {
+        this[Forward] = [];
+        if (this.config.vertical) { this[Forward].push('ArrowDown'); }
+        if (this.config.horizontal) { this[Forward].push('ArrowRight'); }
+    }
+    return this[Forward];
+};
+
+prototypeAccessors.lastIndex.get = function () {
+    return this.elements.length - 1;
+};
+
+prototypeAccessors.validKeys.get = function () {
+    return this.backward.concat(this.forward) || [];
+};
+
+Rover.activate = function activate (el) {
+    if (el.getAttribute('href')) {
+        el.removeAttribute('tabindex');
+    } else {
+        el.setAttribute('tabindex', 0);
+    }
+    return this;
+};
+
+Rover.deactivate = function deactivate (el) {
+    el.setAttribute('tabindex', -1);
+    return this;
+};
+
+Object.defineProperties( Rover.prototype, prototypeAccessors );
 
 export default Rover;
